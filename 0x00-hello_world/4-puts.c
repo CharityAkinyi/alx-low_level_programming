a3ba9c86db5be4ca00e14869489f1efed4586719
CharityAkinyi <charityakinyi38@gmail.com>
2022-03-14 18:25:44 -0700

    task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,1 @@
-#include <stdio.h
-
-/**
- * main - Print a test
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
+latenmmmm.
